85193c58841c1f274b79d2450c494de36cb8f00e
scMarth <vlantaca@gmail.com>
2018-06-25 16:07:08 -0700

    removed redundancies in bls public api example

--- bureau-of-labor-statistics-public-api/blsScript5.py
+++ bureau-of-labor-statistics-public-api/blsScript5.py
@@ -12,45 +12,36 @@ def jsonToStr(json):
     return json.dumps(json)
 
 '''
-
 SeriesID: https://www.bls.gov/help/hlpforma.htm
-
 SMU06415000000000001
-
 SM = State and Area Employment, Hours, and Earnings
 U = Not Seasonally Adjusted
 06 = California
-
 Area Codes
-    41500 = Salinas-Seaside-Monterey
+    41500 = Salinas, CA
     00000 = Statewide
-
 00000000 = Total Nonfarm
 01 = All Employees, In Thousands
-
 '''
+
+apiKey = ""
 
 headers = {'Content-type': 'application/json'}
 data = json.dumps({ \
-    "seriesid": ['SMU06000000000000001'], \
+    "seriesid": [ \
+        'SMU06000000000000001', \
+        'SMU06415000000000001' \
+    ], \
     "startyear":"2000", \
-    "endyear":"2017" \
-    # "registrationkey":"" \
+    "endyear":"2017", \
+    "registrationkey": apiKey \
 })
 p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers)
 cali_data = json.loads(p.text)["Results"]["series"][0]["data"]
+salinas_data = json.loads(p.text)["Results"]["series"][1]["data"]
 
 # print(json.dumps(json_data["Results"]["series"][0]["data"]))
 
-data = json.dumps({ \
-    "seriesid": ['SMU06415000000000001'], \
-    "startyear":"2000", \
-    "endyear":"2017" \
-    # "registrationkey":"" \
-})
-
-p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers)
-salinas_data = json.loads(p.text)["Results"]["series"][0]["data"]
 
 with open('BLS_California-Salinas_2000-2017.csv', 'w') as file:
 
@@ -67,10 +58,4 @@ with open('BLS_California-Salinas_2000-2017.csv', 'w') as file:
         file.write(cali_record["value"] + ",") # statewideValue
         file.write(cali_record["year"]) # year
         file.write("\n")
-        
-
-        
-
-        
-
         
