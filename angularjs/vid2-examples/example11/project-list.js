69781fd38ad3fda248de749d6f0c745ddce8f86e
scMarth <vlantaca@gmail.com>
2018-07-31 09:31:44 -0700

    added angularjs example

---
+++ angularjs/vid2-examples/example11/project-list.js
@@ -0,0 +1,62 @@
+angular.module('project').value('projectListValue', [
+   {
+      name: 'AngularJS',
+      site: 'https://angularjs.org',
+      description: 'HTML enhanced for web apps!'
+   },
+   {
+      name: 'Angular',
+      site: 'https://angular.io',
+      description: 'One framework. Mobile and desktop.'
+   },
+   {
+      name: 'jQuery',
+      site: 'https://jquery.com/',
+      description: 'Write less, do more.'
+   },
+   {
+      name: 'Backbone',
+      site: 'https://backbonejs.org/',
+      description: 'Models for your apps.'
+   },
+   {
+      name: 'SproutCore',
+      site: 'https://sproutcore.com/',
+      description: 'A Framework for Innovative web-apps.'
+   },
+   {
+      name: 'Polymer',
+      site: 'https://www.polymer-project.org/',
+      description: 'Reusable components for the modern web.'
+   },
+   {
+      name: 'Vue.js',
+      site: 'https://vuejs.org/',
+      description: 'The Progressive JavaScript Framework.'
+   },
+   {
+      name: 'Cappucino',
+      site: 'http://www.cappuccino-project.org/',
+      description: 'Objective-J.'
+   },
+   {
+      name: 'Knockout',
+      site: 'http://knockoutjs.com/',
+      description: 'MVVM pattern.'
+   },
+   {
+      name: 'GWT',
+      site: 'http://www.gwtproject.org/',
+      description: 'JS in Java.'
+   },
+   {
+      name: 'Ember',
+      site: 'https://emberjs.com/',
+      description: 'Ambitious web apps.'
+   },
+   {
+      name: 'React',
+      site: 'https://reactjs.org/',
+      description: 'A JavaScript library for building user interfaces.'
+   }
+])
