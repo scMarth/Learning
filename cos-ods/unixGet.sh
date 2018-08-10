5da688c3b3d894956a9b972c7dc43ccf9928ebc5
scMarth <vlantaca@gmail.com>
2018-08-10 14:28:06 -0700

    added unix command examples of using cos ods api

---
+++ cos-ods/unixGet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X GET https://cityofsalinas.opendatasoft.com/api/records/1.0/search/?dataset=anonymized-crime-data
---
+++ cos-ods/unixPost.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -d  'dataset=anonymized-crime-data' https://cityofsalinas.opendatasoft.com/api/records/1.0/search/
