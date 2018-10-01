1eb209460290e2b1065345004d6c6b21f4faf58b
scMarth <vlantaca@gmail.com>
2018-10-01 10:59:29 -0700

    update examples and notes

--- python/functionArgs.py
+++ python/functionArgs.py
@@ -2,3 +2,23 @@ def function(string="Initial Value"):
     print(string)
 
 function("override")
+
+
+def manyArgs(arg1, arg2, arg3, arg4):
+    print("arg1:")
+    print(arg1)
+    print("")
+
+    print("arg2:")
+    print(arg2)
+    print("")
+
+    print("arg3:")
+    print(arg3)
+    print("")
+
+    print("arg4:")
+    print(arg4)
+    print("")
+
+manyArgs("foo", 1, 2, range(1,3))
