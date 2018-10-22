6374433fc4a5a849febc8b2f15e740076eae6ccd
scMarth <vlantaca@gmail.com>
2018-10-22 08:25:06 -0700

    update python multiprocessing examples

--- python/multiprocessing/groups_of_processes3.py
+++ python/multiprocessing/groups_of_processes3.py
@@ -1,11 +1,12 @@
-# This example shows how to limit the number of processes to an arbitrary group size
-# one flaw of this program is that it assumes that the job always finishes..
-# see groups_of_processes3.py for a solution to this issue.
+# This example shows how to limit the number of processes to MAX_GROUP_SIZE
+# it doesn't matter whether the job processes is killed before it joins or not
 
-import multiprocessing, sys
+import multiprocessing, sys, os
 
-def job(q, input):
-    q.put([[i*input for i in range(0,100000)], multiprocessing.current_process().pid])
+def job(q, input_number):
+    if input_number in [3, 5, 16]: # randomly kill some processes
+        os.kill(multiprocessing.current_process().pid, 9)
+    q.put([[i*input_number for i in range(0,100000)], multiprocessing.current_process().pid])
 
 if __name__ == "__main__":
     print("Starting...")
@@ -18,13 +19,11 @@ if __name__ == "__main__":
     MAX_GROUP_SIZE = 5
     input_index = 0
     processes = []
-    num_finished_processes = 0
 
     print("number of processes: " + str(len(processes)))
     print("number of inputs: " + str(len(inputs)))
-    print("")
 
-    while num_finished_processes < len(inputs):
+    while input_index < len(inputs) or len([p for p in processes if p.is_alive()]) or not q.empty():
         if len(multiprocessing.active_children()) < MAX_GROUP_SIZE and input_index < len(inputs):
             p = multiprocessing.Process(target=job, args=(q,inputs[input_index]))
             input_index += 1
@@ -39,9 +38,7 @@ if __name__ == "__main__":
             dead_process = [p for p in processes if p.pid == pid][0]
             dead_process.join()
             dead_process.terminate()
-            num_finished_processes += 1
             process_results.append(result)
 
     print("\nnumber of results: " + str(len(process_results)))
-    print("num_finished_processes: " + str(num_finished_processes))
     print("Done.")
