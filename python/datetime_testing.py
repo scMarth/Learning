cbae9acad5cde6011cce8315f0a527434b13c3b6
scMarth <vlantaca@gmail.com>
2018-12-03 10:46:11 -0800

    added python datetime example

---
+++ python/datetime_testing.py
@@ -0,0 +1,20 @@
+import datetime
+
+# 11/30/18 - 11:08am
+some_date = datetime.datetime(\
+    year=2018, \
+    month=11, \
+    day=30, \
+    hour=11, \
+    minute=8, \
+    second=55, \
+    microsecond=55 \
+)
+
+print(some_date)
+
+# some_date.replace(second=0, microsecond=0)
+# the above won't work because datetime.replace will return a copy
+some_date = some_date.replace(second=0, microsecond=0)
+
+print(some_date)
