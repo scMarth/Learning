ed1fcf31a9201f2460e395938ccd5f0670a045ab
scMarth <vlantaca@gmail.com>
2018-12-03 18:30:42 -0800

    update notes

---
+++ php/example_connect.php
@@ -0,0 +1,12 @@
+<?php
+$serverName = "serverName\\sqlexpress"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"dbName", "UID"=>"userName", "PWD"=>"password");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
