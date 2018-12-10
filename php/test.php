48473668577f3c7bb6180881a4972ffd0abbda1a
scMarth <vlantaca@gmail.com>
2018-12-10 15:58:26 -0800

    added php ex

---
+++ php/test.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<?php
+    $people = array("Alice", "Bob", "Catherine");
+    foreach ($people as $person){
+        echo $person . ' ';
+    }
+
+    echo "<br>";
+    
+    for ($x = 0; $x <= 10; $x++){
+        echo "num: $x<br>";
+    }
+?>
+
+</html>
