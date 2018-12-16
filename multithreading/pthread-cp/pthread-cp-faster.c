cc77375a68fa9b79e3148bb2e1819c4cb496038f
scMarth <vlantaca@gmail.com>
2018-12-15 18:32:38 -0800

    added notes / example for javascript promises

--- multithreading/pthread-cp/pthread-cp-faster.c
+++ multithreading/pthread-cp/pthread-cp-faster.c
@@ -6,8 +6,7 @@
 volatile long int a = 0;
 pthread_mutex_t aLock;
 
-void threadFunc(void *arg)
-{
+void threadFunc(void *arg){
     int i;
     long int localA = 0;
 
@@ -18,11 +17,9 @@ void threadFunc(void *arg)
     pthread_mutex_lock(&aLock);
     a = a + localA;
     pthread_mutex_unlock(&aLock);
-
 }
 
-void threadFunc2(void *arg)
-{
+void threadFunc2(void *arg){
     int i;
     long int localA = 0;
 
@@ -35,8 +32,7 @@ void threadFunc2(void *arg)
     pthread_mutex_unlock(&aLock);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     pthread_t one, two;
     int i;
     a = 0;
