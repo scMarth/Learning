769f22836bfa6b1d65938b54d5c1e49dbb52863c
scMarth <vlantaca@gmail.com>
2018-12-17 15:33:11 -0800

    update python example

--- php/call-python-script/call-python.php
+++ php/call-python-script/call-python.php
@@ -1,9 +1,6 @@
 <?php 
 
-$output=shell_exec('C:\path\python.exe C:\path\test.py one two');
-print_r($output);
-echo "<br>";
-
-echo "hi";
+$output=shell_exec('C:\path\python.exe C:\path\test.py one two 2>&1'); // 2>&1 will also print the error
+echo "<pre>$output</pre>";
 
 ?>
