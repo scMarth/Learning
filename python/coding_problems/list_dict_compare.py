b74bff45b25a1c5d4b62d06362242ee4753d35f6
scMarth <vlantaca@gmail.com>
2019-02-13 14:19:58 -0800

    update arcpy examples, arcgis js examples, sql server notes, python coding problems

---
+++ python/coding_problems/list_dict_compare.py
@@ -0,0 +1,10 @@
+# https://stackoverflow.com/questions/54675348/trying-to-iterate-a-dataframe-and-a-dictionnary-but-it-returns-more-values-than
+
+campaign_names = {
+    'abc' : 1,
+    'dfs' : 2,
+    'zxc' : 3,
+    'tya' : 1
+}
+
+for campaign_name in campaign_names:
