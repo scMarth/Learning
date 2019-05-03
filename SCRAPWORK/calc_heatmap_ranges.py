9010f13f635b240bf0eb031b862367650c48dfea
scMarth <vlantaca@gmail.com>
2019-05-03 13:46:03 -0700

    update example

--- SCRAPWORK/calc_heatmap_ranges.py
+++ SCRAPWORK/calc_heatmap_ranges.py
@@ -1,4 +1,7 @@
-factor = float(14653)/50
+MAXVALUE = 14653
+num_bins = 50
+
+factor = float(MAXVALUE)/num_bins
 
 numbers = [0, 185, 410, 669, 977, 1369, 1883, 2571, 3391, 4785]
 colors = ['#00FF00', '#39FF00', '#71FF00', '#AAFF00', '#E3FF00', '#FFE300', '#FFAA00', '#FF7100', '#FF3900', '#FF0000']
