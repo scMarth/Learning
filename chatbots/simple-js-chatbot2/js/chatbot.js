e4abc931f58cdd008e3bdada3be2b5ae7f1535a9
scMarth <vlantaca@gmail.com>
2019-07-22 14:09:46 -0700

    update chatbot ex, add python ex

--- chatbots/simple-js-chatbot2/js/chatbot.js
+++ chatbots/simple-js-chatbot2/js/chatbot.js
@@ -8,20 +8,723 @@ app.cfg = {
     */
     INITIAL_PROMPT: 'Hello, which of the following did you need help with?',
     CATEGORIES: [
-        'Change password',
-        'Change e-mail'
+        'Airport Influence Area',
+        'Alisal',
+        'Alisal District',
+        'Alisal Vibrancy Plan',
+        'Alternative Transportation',
+        'Bench Mark',
+        'Benchmark',
+        'Bike',
+        'Building',
+        'Centerline',
+        'City of Salinas Boundary',
+        'Community Center',
+        'Community Centers',
+        'Community Development',
+        'Community Services',
+        'Council Districts',
+        'Economic Development',
+        'Emergency Response',
+        'FEMA',
+        'Facilities',
+        'Family Center',
+        'Family Resource Center',
+        'Federal Emergency Management Agency',
+        'Fire',
+        'Fire Department',
+        'Fire House',
+        'Fire Station',
+        'Firefighter',
+        'Flood',
+        'GPS',
+        'GPS Control Network',
+        'General Plan',
+        'Half Mile',
+        'High Performing Government',
+        'Intersection Counts',
+        'Intersections',
+        'Intersections',
+        'KPI',
+        'Landmarks',
+        'Landuse',
+        'Library',
+        'Monument',
+        'NFHL',
+        'NGS',
+        'NTMP',
+        'National Geodetic Survey',
+        'Neighborhood Vibrancy',
+        'Open',
+        'Open Space',
+        'Park',
+        'Parks and Recreation',
+        'Planning',
+        'Planning and Community Engagement',
+        'Police',
+        'Public Works',
+        'Quality of Life',
+        'Recreation',
+        'Recreation Centers',
+        'Road',
+        'Roundabouts',
+        'Safety',
+        'Salinas',
+        'Salinas Municipal Airport',
+        'Space',
+        'Speed Bumps',
+        'Survey',
+        'Survey',
+        'Traffic',
+        'Transportation',
+        'Transportation and Infrastructure',
+        'Urban Design',
+        'Vision Salinas',
+        'Walk',
+        'Water',
+        'Youth Centers',
+        'Zoning'
     ],
 
     /*
     
     A resposne map in the form of an array. Each item in the response map is a list, which contains a pairing of a
-    regular expression which will be searched in the user's input, and a string containing what text should be displayed
-    to the user when that regular expression is matched.
+    regular expression which will be searched in the user's input, and a list of description and links
 
     */
     RESPONSE_MAP: [
-        [/[Ee]\s*-*\s*[Mm][Aa][Ii][Ll]/g, "You want to change your email"],
-        [/[Pp][Aa][Ss][Ss]\s*-*\s*[Ww][Oo][Rr][Dd]/g, "You want to change your password"]
+        [/AIA/ig,
+            [
+                {
+                    description: "Here are Airport Influence Area results from our Open Data Portal:",
+                    link: "https://cityofsalinas.opendatasoft.com/explore/?refine.language=en&sort=modified&q=airport+influence+area"
+                }
+            ]
+        ],
+        [/airport\s*-*\s*influence\s*-*\s*area/ig,
+            [
+                {
+                    description: "Here are results from our Open Data Portal for 'Airport Influence Area':",
+                    link: "https://cityofsalinas.opendatasoft.com/explore/?refine.language=en&sort=modified&q=airport+influence+area"
+                }
+            ]
+        ],
+        [/alisal/ig,
+            [
+                {
+                    description: "Here are results from our Open Data Portal for 'Alisal':",
+                    link: "https://cityofsalinas.opendatasoft.com/explore/?refine.language=en&sort=modified&q=alisal"
+                }
+            ]
+        ],
+        [/alisal\s*-*\s*district/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/alisal\s*-*\s*vibrancy\s*-*\s*plan/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/alternative\s*-*\s*transportation/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/bench\s*-*\s*mark/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/benchmark/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/(bike(path|way|\s*-*\s*plan|\s*-*\s*route)|bicycle)/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/building/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/centerline/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/city\s*-*\s*of\s*-*\s*salinas\s*-*\s*boundary/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/community\s*-*\s*center/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/community\s*-*\s*centers/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/community\s*-*\s*development/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/community\s*-*\s*services/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/council\s*-*\s*districts/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/economic\s*-*\s*development/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/emergency\s*-*\s*response/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/fema/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/facilities/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/family\s*-*\s*center/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/family\s*-*\s*resource\s*-*\s*center/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/federal\s*-*\s*emergency\s*-*\s*management\s*-*\s*agency/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/fire/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/fire\s*-*\s*department/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/fire\s*-*\s*house/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/fire\s*-*\s*station/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/firefighter/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/flood/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/gps/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/gps\s*-*\s*control\s*-*\s*network/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/general\s*-*\s*plan/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/half\s*-*\s*mile/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/high\s*-*\s*performing\s*-*\s*government/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/intersection\s*-*\s*counts/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/intersections/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/intersections/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/kpi/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/landmarks/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/landuse/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/library/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/monument/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/nfhl/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/ngs/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/ntmp/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/national\s*-*\s*geodetic\s*-*\s*survey/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/neighborhood\s*-*\s*vibrancy/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/open/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/open\s*-*\s*space/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/park/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/parks\s*-*\s*and\s*-*\s*recreation/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/planning/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/planning\s*-*\s*and\s*-*\s*community\s*-*\s*engagement/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/police/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/public\s*-*\s*works/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/quality\s*-*\s*of\s*-*\s*life/ig,
+            [
+                {
+                    description: "Here are quality of life results from our Open Data Portal:",
+                    link: "https://cityofsalinas.opendatasoft.com/explore/?refine.language=en&sort=modified&q=quality+of+life"
+                }
+            ]
+        ],
+        [/recreation/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/recreation\s*-*\s*centers/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/road/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/roundabouts/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/safety/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/salinas/ig,
+            [
+                {
+                    description: "Visit the City of Salinas website:",
+                    link: "https://www.cityofsalinas.org/"
+                },
+                {
+                    description: "Visit the City of Salinas Open Data Portal:",
+                    link: "https://cityofsalinas.opendatasoft.com/pages/homepage/"
+                },
+                {
+                    description: "Visit our storymaps:",
+                    link: "https://giswebservices.ci.salinas.ca.us/storymaps/dashboard/"
+                },
+                {
+                    description: "Visit the City of Salinas GIS Division webpage:",
+                    link: "https://www.cityofsalinas.org/our-city-services/public-works/gis-services"
+                },
+                {
+                    description: "View our map gallery:",
+                    link: "https://www.cityofsalinas.org/our-government/information-center/map-gallery"
+                }
+            ]
+        ],
+        [/salinas\s*-*\s*municipal\s*-*\s*airport/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/space/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/speed\s*-*\s*bumps/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/survey/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/survey/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/traffic/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/transportation/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/transportation\s*-*\s*and\s*-*\s*infrastructure/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/urban\s*-*\s*design/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/vision\s*-*\s*salinas/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/walk/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/water/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/youth\s*-*\s*centers/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ],
+        [/zoning/ig,
+            [
+                {
+                    description: "",
+                    link: ""
+                }
+            ]
+        ]
     ]
 };
 
@@ -75,11 +778,18 @@ function fetchResponse(text){
 
     responseMap.forEach(function(regexResponsePair){
         regex = regexResponsePair[0];
-        response = regexResponsePair[1];
+        responseData = regexResponsePair[1];
 
         var found = text.match(regex);
         if (found){
-            returnResponse = response;
+            returnResponse = '';
+            responseData.forEach(function(responseItem){
+                returnResponse += responseItem.description;
+                    if (responseItem.link){
+                        returnResponse += '<br><a href="' + responseItem.link + '" target="_blank">' + responseItem.link + '</a>'
+                    }
+                    returnResponse += '<br>';
+            })
             return;
         }
     });
