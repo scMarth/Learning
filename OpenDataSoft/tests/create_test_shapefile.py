e7db3a3698be22bfbafd789c5240f130c0a0a215
scMarth <vlantaca@gmail.com>
2019-10-09 14:00:47 -0700

    update ods scrapwork

--- OpenDataSoft/tests/create_test_shapefile.py
+++ OpenDataSoft/tests/create_test_shapefile.py
@@ -48,8 +48,8 @@ def createTestShapefile():
         'Date_Day' \
     ]
 
-    x = 0
-    y = 0
+    x = 5784181.434937561
+    y = 2139043.6504749693
 
     data = [ \
         ['Bob', '1', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'],
@@ -73,17 +73,20 @@ def createTestShapefile():
         ['Bob', '1', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'],
         ['1', 'asdf', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # causes Integer error
         ['1', '1 + 1', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # causes Integer error
-        ['1.5', '324.222', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # OK, decimal is truncated
+        ['1.5', '324.222', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # OK, decimal is truncated (LOSS OF DATA)
         ['2', '8', '1.043fds', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # causes Decimal error
-        ['3', '8', '1', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # OK
-        ['4', '8', '1.043', '13/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # OK, swaps month and date
+        ['3', '8', '1', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # OK (putting int in decimal field)
+        ['4', '8', '1.043', '13/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # OK, swaps month and date (POTENTIALLY WRONG)
         ['4.5', '8', '1.043', '13/13/2019 12:24 PM', '2019', '11/2019', '11/6/2019'], # error because can't interpret 13/13 for month / day (both options exceed 12 months)
-        ['5', '8', '1.043', '13/6/2019 14:24', '2019', '11/2019', '11/6/2019'],  # OK 7:24 AM ...????? it subtracted  7...
-        ['6', '8', '1.043', '13/6/2019', '2019', '11/2019', '11/6/2019'],  # OK ... June 12, 2019 5:00 PM
+        ['5', '8', '1.043', '13/6/2019 14:24', '2019', '11/2019', '11/6/2019'],  # OK 7:24 AM ...????? it subtracted  7... (WRONG)
+        ['6', '8', '1.043', '13/6/2019', '2019', '11/2019', '11/6/2019'],  # OK ... June 12, 2019 5:00 PM (WRONG?)
         ['7', '8', '1.043', '11/6/2019 12:24 PM', 'asdf', '11/2019', '11/6/2019'], # causes Date Year error
-        ['8', '8', '1.043', '11/6/2019 12:24 PM', '2019', '11', '11/6/2019'], # OK January 2001
+        ['8', '8', '1.043', '11/6/2019 12:24 PM', '2019', '11', '11/6/2019'], # OK January 2001 (WRONG?? .. Potential data corruption)
+        ['8.25', '8', '1.043', '11/6/2019 12:24 PM', '2019', '13/2019', '11/6/2019'], # Can't convert into a date error
+        ['8.35', '8', '1.043', '11/6/2019 12:24 PM', '2019', 'January 2019', '11/6/2019'], # OK January 2019
+        ['8.45', '8', '1.043', '11/6/2019 12:24 PM', '2019', 'Jan. 2019', '11/6/2019'], # OK November 2019 (WRONG)
         ['8.5', '8', '1.043', '11/6/2019 12:24 PM', '2019', 'asdf', '11/6/2019'], # date_month error
-        ['9', '8', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '2019'], # OK January 1, 2019
+        ['9', '8', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '2019'], # OK January 1, 2019 (WRONG)
         ['9.5', '8', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', 'asdfadsf'], # date_day error
         ['Bob', '1', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019'],
         ['Bob', '19852347825', '1.043', '11/6/2019 12:24 PM', '2019', '11/2019', '11/6/2019']
@@ -91,8 +94,6 @@ def createTestShapefile():
 
     with arcpy.da.InsertCursor(fc, fields) as cursor:
         for record in data:
-            x += 1
-            y += 1
             xy = (x,y)
 
             insert = [xy]
@@ -105,9 +106,12 @@ def createTestShapefile():
 createTestShapefile()
 print('Done.')
 
-# basepath = r'.'
-# shp_path = basepath + r'\testPoints.gdb'
-# fc = shp_path + r'\testPoints'
+'''
 
-# npa = arcpy.da.TableToNumPyArray(fc, field_names='*') # use '*' to get all fields
-# print(npa)
+Type definitions:
+
+https://help.opendatasoft.com/platform/en/publishing_data/05_processing_data/defining_a_dataset_schema.html#choosingtype
+
+Looks like any geospatial data is converted into WGS84. Could be why some polygons that look fine in ArcPro can cause processing errors when uploaded to OpenDataSoft portals.
+
+'''
