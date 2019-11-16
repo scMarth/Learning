deac09dcda651f6178bd226a2305ff55b855132a
scMarth <vlantaca@gmail.com>
2019-11-15 23:47:10 -0800

    update gulp sample project and add sublime notes

--- JavaScript/NodeJS/gulp/sample-project/gulpfile.js
+++ JavaScript/NodeJS/gulp/sample-project/gulpfile.js
@@ -1,64 +1,74 @@
-const { src, dest, parallel, watch, series } = require('gulp');
-const less = require('gulp-less');
-const minifyCSS = require('gulp-csso');
-const concat = require('gulp-concat');
-const terser = require('gulp-terser'); // apparently gulp-uglify-es is deprecated? they say to use this
-const jshint = require('gulp-jshint');
-const clean = require('gulp-clean');
+// required plugins
+const gulp = require('gulp'),
+  less = require('gulp-less'),
+  csso = require('gulp-csso'),
+  concat = require('gulp-concat'),
+  terser = require('gulp-terser'), // apparently gulp-uglify-es is
+                                   // deprecated? they say to use this
+  jshint = require('gulp-jshint'),
+  clean = require('gulp-clean');
 
+// config
 const paths = {
   css: 'src/less/*.less',
   js: 'src/js/**/*.js',
   dest: 'build'
-}
+},
+options = {
+  jshint_reporter: [
+    {
+      verbose: true
+    }
+  ],
+  src: {
+    clean: {
+      allowEmpty: true
+    },
+    js: {
+      sourcemaps: false
+    }
+  },
+  dest: {
+    js: {
+      sourcemaps: false // disable printing of info that can be used to
+                        // map code within compressed file back to its
+                        // original source
+    }
+  },
+  terser: {
+    toplevel: true // mangles top level variable names and drops unused
+                   // functions and variables
+  }
+};
 
-function css() {
-  return src(paths.css)
+// css
+gulp.task('css', () => {
+  return gulp.src(paths.css)
     .pipe(less())
     .pipe(concat('app.styles.min.css'))
-    .pipe(minifyCSS())
-    .pipe(dest('build/css'))
-}
+    .pipe(csso())
+    .pipe(gulp.dest('build/css'))    
+});
 
-function js() {
-  return src(paths.js, { sourcemaps: true })
+// js
+gulp.task('js', () => {
+  return gulp.src(paths.js, options.src.js)
     .pipe(jshint())
-    .pipe(jshint.reporter('default', { verbose: true }))
+    .pipe(jshint.reporter('default', options.jshint_reporter))
     .pipe(concat('app.min.js'))
-    .pipe(terser({
-      toplevel: true
-    }))
-    .pipe(dest('build/js', { sourcemaps: true }))
-}
+    .pipe(terser(options.terser))
+    .pipe(gulp.dest('build/js', options.dest.js))
+});
 
-function watchFiles(){
-  watch(paths.js, js);
-  watch(paths.css, css);
-}
-
-function cleanFiles(){
-  return src(paths.dest, { allowEmpty: true })
+// clean
+gulp.task('clean', () => {
+  return gulp.src(paths.dest, options.src.clean)
     .pipe(clean())
-}
+});
 
-exports.js = js;
-exports.css = css;
-exports.watch = watchFiles;
-exports.clean = cleanFiles;
-exports.default = series(
-  cleanFiles,
-  parallel(css, js)
-);
+gulp.task('watch', function(){
+  gulp.watch(paths.js, gulp.series('js'));
+  gulp.watch(paths.css,  gulp.series('css'));
+});
 
-
-/*
-
-https://github.com/terser/terser#minify-options
-
-Note, concat is called before terser because the 'toplevel' option, which mangles
-top level variable names and drops unused functions and variables
-
-To-Do:
-  jshint
-
-*/
+gulp.task('default', gulp.series('clean', gulp.parallel('css', 'js')));
