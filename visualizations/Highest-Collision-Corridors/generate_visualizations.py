13e61a5453f2f49da7e46b882c588d36f428d392
scMarth <vlantaca@gmail.com>
2020-02-06 13:25:21 -0800

    update visualization example

--- visualizations/Highest-Collision-Corridors/generate_visualizations.py
+++ visualizations/Highest-Collision-Corridors/generate_visualizations.py
@@ -1,5 +1,69 @@
-import os, json
+import os, json, sys
 import pandas as pd
+
+def get_cache_and_fields_from_type_1_df(df):
+    # get the fields
+    fields = {}
+    for i in range(len(df.columns)):
+        field = df.columns[i]
+        if i != 1 and i < (len(df.columns) - 1): # skip the second column because it's empty, skip last column
+            fields[i] = field
+    
+    # get the data
+    cache = []
+    for index, row in df.iterrows():
+        if index < 10: # we're only concerned with the top 10 data
+            insert = {}
+            for i in range(len(row)):
+                if i in fields: # grab value for fields we're interested in
+                    insert[fields[i]] = None if row.isna()[i] else row[i]
+            cache.append(insert)
+
+    return [fields, cache]
+
+def get_cache_and_fields_from_type_2_df(df):
+    fields = {
+        'PCF' : {},
+        'CT' : {}
+    }
+
+    # get pcf fields
+    for i in range(len(df.iloc[0])):
+        val = df.iloc[0][i]
+        if i != 1: # skip the second column because it's empty, skip the last column
+            if val == 'Totals':
+                break
+            fields['PCF'][i] = val
+
+    # get cf fields
+    for i in range(len(df.iloc[25])):
+        val = df.iloc[25][i]
+        if i != 1: # skip the second column because it's empty, skip the last column
+            if val == 'Totals':
+                break
+            fields['CT'][i] = val
+
+    cache = {
+        'PCF' : [],
+        'CT' : []
+    }
+
+    # get pcf data
+    for row_ind in [1, 2, 3]:
+        row = df.iloc[row_ind]
+        insert = {}
+        for i in fields['PCF']:
+            insert[fields['PCF'][i]] = None if row.isna()[i] else row[i]
+        cache['PCF'].append(insert)
+
+    for row_ind in [26, 27, 28]:
+        row = df.iloc[row_ind]
+        insert = {}
+        for i in fields['CT']:
+            insert[fields['CT'][i]] = None if row.isna()[i] else row[i]
+        cache['CT'].append(insert)
+
+    return [fields, cache]
 
 workspace = os.path.dirname(__file__)
 
@@ -9,92 +73,63 @@ excel_file = workspace + '/test.xlsx'
 xl = pd.ExcelFile(excel_file)
 
 # print sheet names
-# print('SHEET NAMES:')
-# print(xl.sheet_names)
-# print('')
-
-# get the data frame for the second sheet
-sheet1_df = xl.parse(1)
-
-# print column names
-# print('COLUMN NAMES:')
-# for column_name in sheet1_df.columns:
-#     print(column_name)
-# print('')
-
-# build the cache for primary collision factors data
-pcf_cache = []
-pcf_fields = {}
-
-for index, row in sheet1_df.iterrows():
-
-    # the first row should have field information
-    if index == 0:
-        for i in range(len(row) - 1): # skip the last item (totals)
-            if i != 1: # skip the second column because it's blank
-                pcf_fields[i] = row[i]
-    elif index >= 1 and index <= 10:
-        insert = {}
-        for i in range(len(row) - 1): # skip the last item (totals)
-            if i != 1: # skip the second column because it's blank
-                insert[pcf_fields[i]] = None if row.isna()[i] else row[i]
-        pcf_cache.append(insert)
-
+print('SHEET NAMES:')
+print(xl.sheet_names)
 print('')
-print('PCF FIELDS:')
-print(pcf_fields)
 
-print('')
-print('PCF DATA:')
-for item in pcf_cache:
-    print(item)
+# mapping from sheet index to sheet name
+sheet_name_map = {}
+for i in range(len(xl.sheet_names)):
+    name = xl.sheet_names[i]
+    sheet_name_map[i] = name
 
-# get the data frame for the third sheet
-sheet2_df = xl.parse(2)
+for sheet_ind in sheet_name_map:
+    # skip the first sheet
+    if sheet_ind == 0:
+        continue
 
-# build the cache for collision type data
-ct_cache = []
-ct_fields = {}
+    sheet_df = xl.parse(sheet_ind)
 
-for index, row in sheet2_df.iterrows():
+    fields = None
+    cache = None
 
-    # the first row should have field information
-    if index == 0:
-        for i in range(len(row) - 1): # skip the last item (totals)
-            if i != 1: # skip the second column because it's blank
-                ct_fields[i] = row[i]
-    elif index >= 1 and index <= 10:
-        insert = {}
-        for i in range(len(row) - 1): # skip the last item (totals)
-            if i != 1: # skip the second column because it's blank
-                insert[ct_fields[i]] = None if row.isna()[i] else row[i]
-        ct_cache.append(insert)
+    if sheet_name_map[sheet_ind] in ['HCC-PCF', 'HCC-Collision Type', 'HCI-PCF', 'HCI-Collision Type']:
+        fields, cache = get_cache_and_fields_from_type_1_df(sheet_df)
 
-print('')
-print('CT FIELDS:')
-print(ct_fields)
+        # output json directory path
+        output_json_dir = workspace + '/json/' + sheet_name_map[sheet_ind]
 
-print('')
-print('CT DATA:')
-for item in ct_cache:
-    print(item)
+        # create the path if it doesn't exist
+        if not os.path.exists(output_json_dir):
+            os.makedirs(output_json_dir)
+        
+        for i in range(len(cache)):
+            curr_ind = i + 1
 
-output_json_dir = workspace + '/json'
-pcf_dir = output_json_dir + '/pcf'
-ct_dir = output_json_dir + '/ct'
+            out_filename = output_json_dir + '/' + str(curr_ind) + '.json'
+            with open(out_filename, 'w') as out_file:
+                json.dump(cache[i], out_file)
 
-for path in [output_json_dir, pcf_dir, ct_dir]:
-    if not os.path.exists(path):
-        os.makedirs(path)
+    elif sheet_name_map[sheet_ind] in ['BikeCorridors', 'Pedestrian', 'SchoolZones', 'AlcoholInvolved', 'UnsafeSpeedCorridors']:
+        # get_cache_and_fields_from_type_2_df(sheet_df)
+        fields, cache = get_cache_and_fields_from_type_2_df(sheet_df)
 
-for i in range(len(pcf_cache)):
-    curr_ind = i + 1
+        # output json directory paths
+        output_json_dir = workspace + '/json/' + sheet_name_map[sheet_ind]
+        pcf_dir = output_json_dir + '/pcf'
+        ct_dir = output_json_dir + '/ct'
 
-    pcf_out_filename = pcf_dir + '/' + str(curr_ind) + '.json'
-    with open(pcf_out_filename, 'w') as pcf_out_file:
-        json.dump(pcf_cache[i], pcf_out_file)
+        # create the paths if they don't exist
+        for path in [output_json_dir, pcf_dir, ct_dir]:
+            if not os.path.exists(path):
+                os.makedirs(path)
 
-    ct_out_filename = ct_dir + '/' + str(curr_ind) + '.json'
-    with open(ct_out_filename, 'w') as ct_out_file:
-        json.dump(ct_cache[i], ct_out_file)
+        # write the pcf data
+        
+
+        
+
+    
+
+
 
