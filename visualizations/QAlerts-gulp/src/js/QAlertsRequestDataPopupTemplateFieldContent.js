a210ca4394a54d066da7caae3da30ce3c65631f7
scMarth <vlantaca@gmail.com>
2020-02-13 11:51:58 -0800

    add WIP gulp integration for visualization example

---
+++ visualizations/QAlerts-gulp/src/js/QAlertsRequestDataPopupTemplateFieldContent.js
@@ -0,0 +1,145 @@
+var QAlertsRequestDataPopupTemplateFieldContent = [{
+    type: "fields",
+    fieldInfos: [
+        {
+            fieldName: "OBJECTID",
+            label: "OBJECTID"
+        },
+        {
+            fieldName: "Shape",
+            label: "Shape"
+        },
+        {
+            fieldName: "id",
+            label: "id"
+        },
+        {
+            fieldName: "master",
+            label: "master"
+        },
+        {
+            fieldName: "addDate",
+            label: "addDate"
+        },
+        {
+            fieldName: "addDateUnix",
+            label: "addDateUnix"
+        },
+        {
+            fieldName: "lastAction",
+            label: "lastAction"
+        },
+        {
+            fieldName: "lastActionUnix",
+            label: "lastActionUnix"
+        },
+        {
+            fieldName: "dept",
+            label: "dept"
+        },
+        {
+            fieldName: "displayDate",
+            label: "displayDate"
+        },
+        {
+            fieldName: "displayLastAction",
+            label: "displayLastAction"
+        },
+        {
+            fieldName: "status",
+            label: "status"
+        },
+        {
+            fieldName: "streetId",
+            label: "streetId"
+        },
+        {
+            fieldName: "streetName",
+            label: "streetName"
+        },
+        {
+            fieldName: "streetNum",
+            label: "streetNum"
+        },
+        {
+            fieldName: "crossStreetId",
+            label: "crossStreetId"
+        },
+        {
+            fieldName: "crossStreetName",
+            label: "crossStreetName"
+        },
+        {
+            fieldName: "cityId",
+            label: "cityId"
+        },
+        {
+            fieldName: "cityName",
+            label: "cityName"
+        },
+        {
+            fieldName: "district",
+            label: "district"
+        },
+        {
+            fieldName: "comments",
+            label: "comments"
+        },
+        {
+            fieldName: "privateNotes",
+            label: "privateNotes"
+        },
+        {
+            fieldName: "submitter",
+            label: "submitter"
+        },
+        {
+            fieldName: "typeId",
+            label: "typeId"
+        },
+        {
+            fieldName: "typeName",
+            label: "typeName"
+        },
+        {
+            fieldName: "priorityValue",
+            label: "priorityValue"
+        },
+        {
+            fieldName: "latitude",
+            label: "latitude"
+        },
+        {
+            fieldName: "longitude",
+            label: "longitude"
+        },
+        {
+            fieldName: "aggregatorId",
+            label: "aggregatorId"
+        },
+        {
+            fieldName: "aggregatorInfo",
+            label: "aggregatorInfo"
+        },
+        {
+            fieldName: "origin",
+            label: "origin"
+        },
+        {
+            fieldName: "priorityToDisplay",
+            label: "priorityToDisplay"
+        },
+        {
+            fieldName: "dateClosedUnix",
+            label: "dateClosedUnix"
+        },
+        {
+            fieldName: "resumeDate",
+            label: "resumeDate"
+        },
+        {
+            fieldName: "displayDateClosed",
+            label: "displayDateClosed"
+        }
+    ]
+}]
