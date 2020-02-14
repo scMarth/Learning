3bb9099f8dab0d08b36ce7da3185e98d16921413
scMarth <vlantaca@gmail.com>
2020-02-13 18:38:55 -0800

    remove folder with bad dependency

--- JavaScript/NodeJS/gulp/sample-server-project/src/js/1.js
+++
@@ -1,6 +0,0 @@
-let test = "The quick brown fox";
-let second = "jumps over the lazy dog";
-
-function saySomething(test, second){
-    console.log(test + " " + second);
-}
--- JavaScript/NodeJS/gulp/sample-server-project/src/js/2.js
+++
@@ -1,9 +0,0 @@
-saySomething(test, second);
-console.log('What it do playa');
-console.log('Hey alright');
-
-$.getJSON("./json/data.json", function(data){
-    console.log(data);
-}).fail(function(){
-    console.log('error getting data.json');
-});
