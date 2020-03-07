39504ccfa3151a4d25030731973a63d1bfbf0f82
scMarth <vlantaca@gmail.com>
2020-03-06 22:30:58 -0800

    update notes

---
+++ python/dict/docstring_test.py
@@ -0,0 +1,23 @@
+print('Hello')
+
+my_dict = {
+    '''
+        am i a comment?
+    '''
+
+    'key1' : 'val1',
+    'key2' : 'val2',
+    'key3' : 'val3',
+    'key4' : 'val4',
+    'key5' : 'val5'
+}
+
+
+'''
+
+print('am i a comment?')
+
+'''
+
+print('World\n')
+print(my_dict)
