1d204089656936e3ebcb26d3c11da11ff9a57289
scMarth <vlantaca@gmail.com>
2020-03-12 20:21:55 -0700

    update book list

--- Books/sort_book_list.py
+++ Books/sort_book_list.py
@@ -3,10 +3,13 @@ import csv, os
 book_list_path = os.path.dirname(os.path.abspath(__file__)) + '/Book-List2.csv'
 
 rows = []
+column_names = []
 
 with open(book_list_path, 'r', newline='') as csvfile:
    csvreader = csv.reader(csvfile) 
    for row in csvreader:
+      if csvreader.line_num == 1:
+         column_names = row
       if csvreader.line_num > 1:
          rows.append(row)
 
@@ -14,7 +17,7 @@ rows.sort(key=lambda x: (x[1], x[0]))
 
 with open(book_list_path, 'w', newline='', encoding='utf-8') as csvfile:
    csvwriter = csv.writer(csvfile, lineterminator='\n')
-   csvwriter.writerow(['Title', 'Author'])
+   csvwriter.writerow(column_names)
    for row in rows:
       csvwriter.writerow(row)
 
