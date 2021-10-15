05409be357d259452ec862e2e209e835e964eacb
scMarth <vlantaca@gmail.com>
2021-10-15 10:04:51 -0700

    add example batch script

---
+++ Windows/Windows-Batch-Scripting/open_stuff.bat
@@ -0,0 +1,10 @@
+start "" "C:\Users\User\Documents\Github\mp3gainBatchNormalizer"
+start "" "C:\Users\User\Documents\Github\youtube-dl-helper"
+start "" "C:\Users\User\Documents\Github\spotify_album_info_grabber"
+start "" "C:\Users\User\Videos\YTD"
+start "" "C:\Users\User\Music\User's Music"
+
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/playlist?list=WL"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" "C:\Program Files (x86)\Audacity\audacity.exe"
+
