d74d0590a11bbb6e501d1e5c722f3bdc706bb745
scMarth <vlantaca@gmail.com>
2022-09-01 16:11:43 -0700

    update arcgis and python notes

---
+++ python/file_size_scanner/file_size_scanner.py
@@ -0,0 +1,15 @@
+import os
+
+def get_dir_size(path='.'):
+    total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_dir_size(entry.path)
+    return total
+
+
+path = r'C:\Users\vincent.lantaca\Documents\Github'
+print(get_dir_size(path))
