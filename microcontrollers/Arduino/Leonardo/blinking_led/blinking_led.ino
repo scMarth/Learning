1edcb5f53faefbe4f67df3658d00c06ef57d5a2c
scMarth <vlantaca@gmail.com>
2023-06-06 20:35:52 -0700

    update arduino leonardo notes

--- microcontrollers/Arduino/Leonardo/blinking_led/blinking_led.ino
+++ microcontrollers/Arduino/Leonardo/blinking_led/blinking_led.ino
@@ -1,6 +1,5 @@
-
-
 #include <Keyboard.h>
+#include <time.h>
 
 int led = 13;     // L LED
 
@@ -9,25 +8,27 @@ void setup() {
    // pullup resistor so it goes high unless
    // connected to ground:
    pinMode(2, INPUT_PULLUP);
+   pinMode(3, INPUT_PULLUP);
+   pinMode(4, INPUT_PULLUP);
    pinMode(led, OUTPUT);
    Keyboard.begin();
+   srand(time(NULL));   // Initialization, should only be called once.
 }
 
 void loop() {
-   digitalWrite(led, !digitalRead(2));
-   //if the button is pressed
-   if (digitalRead(2) == LOW) {
-      //Send the message
+   digitalWrite(led, !(digitalRead(2)||digitalRead(4)));
+
+   if (digitalRead(2) == HIGH && digitalRead(3) == HIGH && digitalRead(4) == LOW){
+      // print random letter
+      int r = rand() % 94;
+      r += 32;
+      Keyboard.print((char)r);
+      delay(120); // 1 word ~ 5 chars, 500 chars per minute => 1 char per 60/500 seconds  ~ 120 milliseconds
+   } else if (digitalRead(2) == HIGH && digitalRead(3) == LOW && digitalRead(4) == HIGH){
+      // Keyboard.write(13); // send the "Enter" key // doesn't work
+      Keyboard.write(KEY_RETURN); // send the "Enter" key
+      delay(100);
+   } else if (digitalRead(2) == LOW && digitalRead(3) == HIGH && digitalRead(4) == HIGH){
       Keyboard.print("q");
    }
 }
-
-/*
-
-Notes and Warnings
-
-https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardprint/
-
-When you use the Keyboard.print() command, the Arduino takes over your keyboard! Make sure you have control before you use the command. A pushbutton to toggle the keyboard control state is effective.
-
-*/
